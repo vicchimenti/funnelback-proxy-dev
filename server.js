0d4b3f30dbd48ecd0f0bc9c6788c4f7fc6b6cbdd
Vic Chimenti <chimenti@seattleu.edu>
2025-02-04 05:23:12 -0800

    create server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({ origin: 'https://www.seattleu.edu' }));
+
+app.get('/proxy/funnelback', async (req, res) => {
+    try {
+        const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        const funnelbackUrl = 'https://dxp-us-search.funnelback.squiz.cloud/s/search.html';
+
+        const response = await axios.get(funnelbackUrl, {
+            params: req.query,
+            headers: { 'X-Forwarded-For': userIp }
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(3000, () => console.log('Proxy running on port 3000'));
