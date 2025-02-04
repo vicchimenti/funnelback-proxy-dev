5560e16e932ae35b33715d5ad3152513959ec075
Vic Chimenti <victor.chimenti@gmail.com>
2025-02-04 13:05:39 -0800

    add serch endpoint

---
+++ api/search.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.seattleu.edu');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
+    try {
+        const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        const funnelbackUrl = 'https://dxp-us-search.funnelback.squiz.cloud/s/search.html';
+        
+        const response = await axios.get(funnelbackUrl, {
+            params: req.query,
+            headers: {
+                'Accept': 'text/html',
+                'X-Forwarded-For': userIp
+            }
+        });
+
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).send('Search error: ' + (error.response?.data || error.message));
+    }
+}
+
+module.exports = handler;
