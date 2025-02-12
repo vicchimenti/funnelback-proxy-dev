4c765235390510e9f1286f1c215c580ed1d78419
Vic Chimenti <victor.chimenti@gmail.com>
2025-02-12 14:22:39 -0800

    fix cors error programs

--- api/suggestPrograms.js
+++ api/suggestPrograms.js
@@ -5,7 +5,7 @@
  * Provides real-time search suggestions for academic programs.
  * 
  * @author Victor Chimenti
- * @version 1.0.0
+ * @version 1.1.0
  * @license MIT
  */
 
@@ -34,13 +34,17 @@ function logEvent(level, message, data = {}) {
 
 async function handler(req, res) {
     const startTime = Date.now();
-    const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const requestId = req.headers['x-vercel-id'] || Date.now().toString();
 
+    // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', 'https://www.seattleu.edu');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+    const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
     if (req.method === 'OPTIONS') {
+        logEvent('info', 'OPTIONS request', { requestId, headers: req.headers });
         res.status(200).end();
         return;
     }
@@ -49,16 +53,20 @@ async function handler(req, res) {
         const funnelbackUrl = 'https://dxp-us-search.funnelback.squiz.cloud/s/suggest.json';
         const queryParams = { ...req.query, collection: 'seattleu~ds-programs' };
 
+        logEvent('info', 'Request received', { requestId, query: queryParams, headers: req.headers });
+
         const response = await axios.get(funnelbackUrl, {
             params: queryParams,
-            headers: { 'Accept': 'application/json', 'X-Forwarded-For': userIp }
+            headers: {
+                'Accept': 'application/json',
+                'X-Forwarded-For': userIp
+            }
         });
 
         logEvent('info', 'Response received', {
             status: response.status,
             processingTime: `${Date.now() - startTime}ms`,
-            suggestionsCount: response.data.length || 0,
-            query: queryParams
+            suggestionsCount: response.data.length || 0
         });
 
         res.json(response.data);
